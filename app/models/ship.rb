24f8fe4cd6d58b9908fcc1103e567b6a37501123
Josh Pann <josh.pann@example.com>
2016-12-28 22:13:45 -0800

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+  end
+
+  def self.all
+    SHIP
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
